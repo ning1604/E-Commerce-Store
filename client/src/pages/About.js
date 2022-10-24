999bfddedde5698f2f3f483c5a5735034905e3ed
Ning <100749559+ning1604@users.noreply.github.com>
2022-10-24 19:05:01 +1100

    updated about page content and styling

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,7 +1,11 @@
 const About = () => {
     return (
-        <div className="main-content">
-           <h1>About page</h1> 
+        <div className="main-content page-container">
+            <h1 className="sub-header">About Us</h1>
+            <div className="about-content">
+                <p className="about-text">Description Lorem ipsum dolor sit amet, consectetur adipiscing elit. ALiquam sodales in nisi sit amet finibus. Aenean sed accumsan nisi, vestibulum bibendum dui.</p>
+                <img className="about-img" src="/images/aboutPage.jpg" alt="people placing hands on the trunk of tree"></img>
+            </div>
         </div>
     )
 }
