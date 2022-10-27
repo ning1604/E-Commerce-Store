0428d785eda4dcea76a72cec8cabde7d169f9eeb
Ning <100749559+ning1604@users.noreply.github.com>
2022-10-27 14:55:19 +1100

    added success page for after completing payment

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import { StoreProvider } from './utils/GlobalState';
 import SingleProduct from './pages/SingleProduct';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Success from './pages/Success';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -69,6 +70,10 @@ function App() {
               <Route
                 path='/signup'
                 element={<Signup />}
+              />
+              <Route
+                path='/success'
+                element={<Success />}
               />
               <Route
                 path='*'
