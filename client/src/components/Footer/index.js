b42e6f377c32626f992bf7381c835801dcadcf9e
Ning <100749559+ning1604@users.noreply.github.com>
2022-10-27 20:23:22 +1100

    updated homepage styling

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,9 +6,9 @@ function Footer() {
             <a href='https://github.com/ning1604/E-Commerce-Store' target='_blank' rel='noopener noreferrer' className='email-link'>deterre@deterre.com</a>
             <div className='sec-contacts'>
                 <div className='icon-group'>
-                    <a href='https://github.com/ning1604/E-Commerce-Store' target='_blank' rel='noopener noreferrer'><i className='fa-brands fa-instagram fa-4x'></i></a>
-                    <a href='https://github.com/ning1604/E-Commerce-Store' target='_blank' rel='noopener noreferrer'><i className='fa-brands fa-square-facebook fa-4x'></i></a>
-                    <a href='https://github.com/ning1604/E-Commerce-Store' target='_blank' rel='noopener noreferrer'><i className='fa-brands fa-youtube fa-4x'></i></a>
+                    <a href='https://github.com/ning1604/E-Commerce-Store' target='_blank' rel='noopener noreferrer'><i className='fa-brands fa-instagram fa-3x'></i></a>
+                    <a href='https://github.com/ning1604/E-Commerce-Store' target='_blank' rel='noopener noreferrer'><i className='fa-brands fa-square-facebook fa-3x'></i></a>
+                    <a href='https://github.com/ning1604/E-Commerce-Store' target='_blank' rel='noopener noreferrer'><i className='fa-brands fa-youtube fa-3x'></i></a>
                 </div>
                 <div>
                    <p>+61 491 570 156</p> 
