d582b52f0228be49ea156575a3688525d986d7b8
Ning <100749559+ning1604@users.noreply.github.com>
2022-10-30 21:54:59 +1100

    updated navbar to have hamburger menu

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Auth from '../../utils/auth';
 import Cart from '../Cart';
 import { Link } from 'react-router-dom';
+import Hamburger from 'hamburger-react';
 
 function Navbar() {
+    const [isOpen, setOpen] = useState(false);
     if (Auth.loggedIn()) {
         return (
             <div className='header'>
                 <ul className='nav'>
-                    <li>
+                <Hamburger toggled={isOpen} toggle={setOpen} />
+                    <div className={(isOpen ? 'flex' : 'hidden') + ' nav-menu'}>
+                    <Link to='/shop'>SHOP</Link>
+                    <Link to='/about'>ABOUT</Link>
+                    <Link to='/' onClick={() => Auth.logout()}>LOGOUT</Link>
+                    </div>
+                    <li className='hide-link'>
                         <Link to='/shop'>SHOP</Link>
                     </li>
-                    <li>
+                    <li className='hide-link'>
                         <Link to='/about'>ABOUT</Link>
                     </li>
                 </ul>
                 <Link to='/' className='logo'>ⴲ</Link>
                 <ul className='nav'>
-                    <li>
+                    <li className='hide-link'>
                         <Link to='/' onClick={() => Auth.logout()}>LOGOUT</Link>
                     </li>
                     <li>
@@ -30,16 +38,22 @@ function Navbar() {
         return (
             <div className='header'>
                 <ul className='nav'>
-                    <li>
+                    <Hamburger toggled={isOpen} toggle={setOpen} />
+                    <div className={(isOpen ? 'flex' : 'hidden') + ' nav-menu'}>
+                    <Link to='/shop'>SHOP</Link>
+                    <Link to='/about'>ABOUT</Link>
+                    <Link to='/login'>LOGIN</Link>
+                    </div>
+                    <li className='hide-link'>
                         <Link to='/shop'>SHOP</Link>
                     </li>
-                    <li>
+                    <li className='hide-link'>
                         <Link to='/about'>ABOUT</Link>
                     </li>
                 </ul>
                 <Link to='/' className='logo'>ⴲ</Link>
                 <ul className='nav'>
-                    <li>
+                    <li className='hide-link'>
                         <Link to='/login'>LOGIN</Link>
                     </li>
                     <li>
